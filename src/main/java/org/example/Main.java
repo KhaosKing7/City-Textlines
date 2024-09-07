50bfdbd045a4062c5feadf95710ac5fb69bd0d36
KhaosKing7 <ymeltaif@gmail.com>
2024-09-07 15:07:30 -0700

    finished base game
    i implemented building, income, demolishing and many other features. i need to work on balancing now and the event system, saving and loading. and maybe flesh out the builing system with more building types instead of 3 "areas" that you can build

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,16 @@ import java.util.Random;
 import org.example.gameInstance;
 
 import java.lang.Math;
+import java.util.function.DoubleToIntFunction;
 
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
     public static Random r = new Random();
     static gameInstance game;
-
+    public String actionID;
+    public String actionID2;
+    public int actions = 0;
     public static void main(String[] args) {
         Main main = new Main();
         main.mainMenu();
@@ -125,7 +128,7 @@ public class Main {
             //basically you cant do 2 things (important things) like building stuff
             // however things like making a decision on an event doesn't count towards the action count
             //not implemented im just yapping
-            int actions = 0;
+
 
             gameMainMenu();
         }
@@ -136,19 +139,171 @@ public class Main {
         System.out.flush();
         System.out.println(game.cityName);
         game.printDate();
+        System.out.println("Actions made: "+ actions);
+        System.out.println("Money: " + game.money);
+        System.out.println("Income: "+ game.income);
+        System.out.println("Expenses: "+ game.expenses);
+        System.out.println("Population: " +game.population);
+        System.out.println("Population capacity:"+game.populationCapacity);
+        System.out.println("____________________________________________________________");
         System.out.println("1: build/demolish");
         System.out.println("2: decisions");
+        System.out.println("i: Information");
+        System.out.println("n: Next Turn");
         System.out.println("q: quit");
         String choice = sc.nextLine();
         switch (choice) {
+            //i have no idea how tf to do this :sob:
             case "1":
-                break;
-            case "2":
+                buildMenu();
+            break;
+            case "n":
+                game.nextStep(actionID, actionID2);
+                actions = 0;
                 break;
             case "q":
+                mainMenu();
                 break;
-            default:
+            case "i":
+                System.out.println("Water Provided: "+game.waterProvided);
+                System.out.println("Water Needed: "+ game.waterNeeded);
+                System.out.println("Electricity Produced:"+game.electricityProvided);
+                System.out.println("Electricity Needed:" + game.electricityNeeded);
+                System.out.println("Usable Residential Areas: "+game.maxResArea);
+                System.out.println("Residential Areas: " + game.resArea);
+                System.out.println("Commercial Areas: "+game.comArea);
+                System.out.println("Industrial Areas: "+game.indArea);
+                sc.nextLine();
+                gameMainMenu();
+                break;
+        }
+    }
+
+    public void buildMenu(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("1: Build");
+        System.out.println("2: demolish");
+        System.out.println("q: quit");
+        String choice = sc.nextLine();
+        switch(choice){
+            case "1" -> buildChoices();
+            case "2" -> demolishChoices();
+            case"q" -> gameMainMenu();
+
+        }
+
+    }
+    public void buildChoices(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("1: Residential area");
+        System.out.println("2: Commercial area");
+        System.out.println("3: Industrial area");
+        String choice = sc.nextLine();
+        switch(choice){
+            case "1":
+                if(actions==0){
+                    actionID = "resArea";
+                    actions++;
+                    game.money = game.money-100;
+                }else if(actions==1){
+                    actionID2 = "resArea";
+                    actions++;
+                    game.money = game.money-100;
+                }else {
+                    System.out.println("You have already made 2 actions!");
+                    gameMainMenu();
+                }
+                break;
+            case "2":
+                if(actions==0){
+                    actionID = "comArea";
+                    actions++;
+                    game.money = game.money-100;
+                }else if(actions==1){
+                    actionID2 = "comArea";
+                    actions++;
+                    game.money = game.money-100;
+                }else {
+                    System.out.println("You have already made 2 actions!");
+                    gameMainMenu();
+                }
+                break;
+            case "3":
+                if(actions==0){
+                    actionID = "indArea";
+                    actions++;
+                    game.money = game.money-100;
+                }else if(actions==1){
+                    actionID2 = "indArea";
+                    actions++;
+                    game.money = game.money-100;
+                }else {
+                    System.out.println("You have already made 2 actions!");
+                    gameMainMenu();
+                }
+                break;
+
+        }
+    }
+    public void demolishChoices(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("What would you like to demolish?");
+        System.out.println("1: Residential Area");
+        System.out.println("2: Commercial area");
+        System.out.println("3: Industrial area");
+        System.out.println("q: quit");
+        String choice = sc.nextLine();
+
+        switch(choice){
+            case "1":
+                if(game.buildings.contains("resArea")){
+                    for (int i = 0; i<game.buildings.size(); i++){
+                        if(game.buildings.get(i)=="resArea"){
+                            game.buildings.remove(i);
+                            System.out.println("You have demolished a Residential area");
+                            break;
+                        }
+                    }
+                }else {
+                    demolishChoices();
+                    System.out.println("You have no Residential Areas.");
+                }
+
+
+
+                break;
+            case "2":
+                if(game.buildings.contains("comArea")){
+                    for (int i = 0; i<game.buildings.size(); i++){
+                        if(game.buildings.get(i)=="comArea"){
+                            game.buildings.remove(i);
+                            System.out.println("You have demolished a Commercial area");
+                            break;
+                        }
+                    }
+                }else {
+                    demolishChoices();
+                    System.out.println("You have no Commercial Areas.");
+                }
+                break;
+            case "3":
+                if(game.buildings.contains("indArea")){
+                    for (int i = 0; i<game.buildings.size(); i++){
+                        if(game.buildings.get(i)=="indArea"){
+                            game.buildings.remove(i);
+                            System.out.println("You have demolished a Industrial area");
+                            break;
+                        }
+                    }
+                }else {
+                    demolishChoices();
+                    System.out.println("You have no Commercial Areas.");
+                }
                 break;
         }
     }
 }
+
--- src/main/java/org/example/gameInstance.java
+++ src/main/java/org/example/gameInstance.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Random;
 public class gameInstance {
     public int year;
     public int month = 1;
     public int day = 1;
     public int population;
+    public int populationCapacity;
     public int money = 10000;
     public int income;
     public int waterNeeded;
@@ -13,7 +16,13 @@ public class gameInstance {
     public int electricityProvided;
     public String monthString;
     public String cityName;
-
+    public int resArea = 1;
+    public int comArea = 1;
+    public int indArea = 1;
+    public int maxResArea;
+    public int expenses;
+    ArrayList<String> buildings = new ArrayList<String>();
+    Random r = new Random();
     public gameInstance(int year, String city) {
         this.year = year;
         this.cityName = city;
@@ -23,16 +32,62 @@ public class gameInstance {
         //changes date to the next possible date
         //usually at the middle or beginning of a month
         int halfWayPoint = month == 2 ? 14 : 15;
-        if(day >= halfWayPoint){
+        if (day >= halfWayPoint) {
             month = (month % 12) + 1;
             day = 1;
-        }else{
+        } else {
             day = halfWayPoint;
         }
 
-
 //deals with income, actions, building
+       if(actionID=="resArea" || actionID=="comArea" || actionID == "indArea"){
+           buildings.add(actionID);
+       }
+        if(actionID2=="resArea" || actionID2=="comArea" || actionID2 == "indArea"){
+            buildings.add(actionID2);
+        }
+       //counting how much of each area the player has and calculating stats based on that, adding money, population, electricity production ect ect.
 
+        if(buildings.contains("resArea")){
+            for (int i = 0; i<buildings.size(); i++){
+                if(buildings.get(i)=="resArea"){
+                   resArea++;
+                }
+            }
+        }
+        if(buildings.contains("comArea")){
+            for (int i = 0; i<buildings.size(); i++){
+                if(buildings.get(i)=="comArea"){
+                    comArea++;
+                }
+            }
+        }
+        if(buildings.contains("indArea")){
+            for (int i = 0; i<buildings.size(); i++){
+                if(buildings.get(i)=="indArea"){
+                    indArea++;
+                }
+            }
+        }
+        electricityProvided = indArea*10;
+        waterProvided = indArea*5;
+maxResArea = indArea*5;
+      if(resArea<=maxResArea){
+          populationCapacity = resArea*1000;
+        }else {
+          populationCapacity = maxResArea*1000;
+      }
+income = population*comArea/2;
+electricityNeeded = resArea;
+waterNeeded = resArea*2;
+expenses = resArea*50+indArea*100;
+money = money+income-expenses;
+
+//add a random amount of people to the city
+        int popAdd = r.nextInt(1,100);
+        if((popAdd+population)<populationCapacity){
+            population = popAdd+population;
+        }
     }
 
     public void printDate() {
